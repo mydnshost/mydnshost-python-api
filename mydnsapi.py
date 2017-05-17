da3cdf98ac0e7a4eb6a1a100df16d42a29d386fb
Chris Smith <chris@chameth.com>
2017-05-17 13:05:40 +0100

    Add user related methods.

--- mydnsapi.py
+++ mydnsapi.py
@@ -3,6 +3,13 @@ import requests.auth
 import urllib.parse
 
 
+def authenticated(method):
+    """Decorator that checks that an authenticator has been supplied. Returns None otherwise."""
+    def wrapper(*args):
+        return method(*args) if args[0].has_auth() else None
+    return wrapper
+
+
 class MyDNSHostAPI:
 
     def __init__(self, base_url=None, auth=None):
@@ -14,16 +21,61 @@ class MyDNSHostAPI:
     def set_auth(self, auth: requests.auth.AuthBase):
         self.__auth = auth
 
+    def has_auth(self):
+        return self.__auth is not None
+
     def valid_auth(self):
         return self.get_user_data() is not None
 
+    def use_domain_admin(self, domain_admin):
+        self.__domain_admin = domain_admin
+
+    @authenticated
     def get_user_data(self):
-        if not self.__auth:
-            return None
+        return self.__get('userdata').get('response')
 
-        result = self.__get('userdata')
-        return result['response'] if 'response' in result else None
+    @authenticated
+    def get_users(self):
+        return self.__get('users').get('response')
 
+    @authenticated
+    def get_user_info(self, user_id='self'):
+        return self.__get('users/%s' % user_id).get('response')
+
+    @authenticated
+    def set_user_info(self, data, user_id='self'):
+        return self.__post('users/%s' % user_id, data).get('response')
+
+    @authenticated
+    def create_user(self, data):
+        return self.__post('users/create', data)
+
+    @authenticated
+    def delete_user(self, user_id):
+        return self.__delete('users/%s' % user_id)
+
+    @authenticated
+    def get_api_keys(self):
+        return self.__get('users/self/keys').get('response')
+
+    @authenticated
+    def create_api_key(self, data):
+        return self.__post('users/self/keys', data)
+
+    @authenticated
+    def update_api_key(self, key, data):
+        return self.__post('users/self/keys/%s' % key, data)
+
+    @authenticated
+    def delete_api_key(self, key):
+        return self.__delete('users/self/keys/%s' % key)
+
+    @authenticated
+    def get_session_id(self):
+        return (self.__get('session').get('response') or {}).get('session')
+
+    # TODO: Wrap the requests calls and handle API errors.
+    # TODO: Return just the 'response' value instead of all the callers doing it.
     def __get(self, api_method):
         return requests.get(self.__build_url(api_method), auth=self.__auth).json()
 
@@ -34,7 +86,8 @@ class MyDNSHostAPI:
         return requests.delete(self.__build_url(api_method), auth=self.__auth).json()
 
     def __build_url(self, api_method):
-        return urllib.parse.urljoin(self.__base_url, '%s/%s' % (self.__version, api_method))
+        admin_prefix = 'admin/' if self.__domain_admin and api_method.startswith('domain') else ''
+        return urllib.parse.urljoin(self.__base_url, '%s%s/%s' % (admin_prefix, self.__version, api_method))
 
 
 class UserKeyAuthenticator(requests.auth.AuthBase):
